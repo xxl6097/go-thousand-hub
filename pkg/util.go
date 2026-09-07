ab76286261f32db5529985bdd983c4e943594412
xxl6097 <263996097@qq.com>
2026-09-07 18:08:03 +0800

    v0.0.6 by uuxia 2026-09-07 18:08:03

---
+++ pkg/util.go
@@ -0,0 +1,17 @@
+package pkg
+
+import (
+	"context"
+	"log"
+
+	"github.com/xxl6097/go-thousand-hub/internal/agent"
+	"github.com/xxl6097/go-thousand-hub/pkg/qjt"
+)
+
+func New(opts qjt.Options, ctx context.Context) error {
+	if err := agent.New(opts).Run(ctx); err != nil {
+		log.Printf("agent 退出: %v", err)
+		return err
+	}
+	return nil
+}
