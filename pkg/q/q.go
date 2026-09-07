1a7eb2ddf68d501619805d341f853fafaca7792e
xxl6097 <263996097@qq.com>
2026-09-07 18:14:52 +0800

    v0.0.8 by uuxia 2026-09-07 18:14:52

--- pkg/q/q.go
+++ pkg/q/q.go
@@ -2,14 +2,18 @@ package q
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	"github.com/xxl6097/go-thousand-hub/internal/agent"
 	"github.com/xxl6097/go-thousand-hub/pkg/qjt"
 )
 
-func NewQJT(opts qjt.Options, ctx context.Context) error {
-	if err := agent.New(opts).Run(ctx); err != nil {
+func NewQJT(opts *qjt.Options, ctx context.Context) error {
+	if opts == nil {
+		return errors.New("opts is nil")
+	}
+	if err := agent.New(*opts).Run(ctx); err != nil {
 		log.Printf("agent 退出: %v", err)
 		return err
 	}
